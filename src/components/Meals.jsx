9d42c391f1193fc15782d9b7d0f572dbf07ed6c7
Mohamed Tanash <tanashkoo@gmail.com>
2022-08-21 23:26:20 +0200

    add meals fetching logic

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,14 +1,50 @@
 import styles from "../styles/components/Meals.module.css";
 import globalStyles from "../styles/global.module.css";
-import meals from "../data/meals";
 import Meal from "./Meal";
+import { useEffect, useState } from "react";
+import axiosInstance from "../api/axios";
 
 function Meals() {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    const controller = new AbortController();
+    let subscribed = true;
+    const getMeals = async () => {
+      try {
+        const response = await axiosInstance.get("/meals", {
+          signal: controller.signal,
+        });
+        if (subscribed) {
+          setMeals(response.data);
+          setError(null);
+        }
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      }
+      setLoading(false);
+    };
+
+    getMeals();
+
+    return () => {
+      controller.abort();
+      subscribed = false;
+    };
+  }, []);
+
   return (
     <section className={`${styles.meals} ${globalStyles.container}`}>
-      {meals.map((meal) => (
-        <Meal key={meal.id} {...meal} />
-      ))}
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {!loading &&
+        !error &&
+        meals.map((meal) => <Meal key={meal._id} {...meal} />)}
     </section>
   );
 }
