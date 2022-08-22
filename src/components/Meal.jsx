64ea96007618f82d0b4623b6bca3c3a6eea92fcb
Mohamed Tanash <tanashkoo@gmail.com>
2022-08-22 12:40:51 +0200

    add animation to meal component

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -7,11 +7,18 @@ import truncateString from "../helpers/truncateString";
 import { useContext, useState } from "react";
 import { CartContext } from "../context/cartContext";
 import toastNotify from "../helpers/toast";
+import { useSpring, animated } from "react-spring";
 
 function Meal({ _id, name, description, picture, price }) {
   const [mealAmount, setMealAmount] = useState(1);
 
   const { addItemToCart } = useContext(CartContext);
+
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    delay: 200,
+  });
 
   const handleMealAmountChange = (e) => {
     if (+e.target.value < 1) {
@@ -53,36 +60,38 @@ function Meal({ _id, name, description, picture, price }) {
   };
 
   return (
-    <Card className={styles.meal}>
-      <img src={picture} alt={name} />
-      <h3 className={styles.mealName}>{name}</h3>
-      <p className={styles.mealDescription}>{truncateString(description)}</p>
-      <p className={styles.mealPrice}>${price.toFixed(2)}</p>
-      <div className={styles.mealAddSection}>
-        <div className={styles.mealAmount}>
-          <label htmlFor="number" defaultValue="1">
-            Amount
-          </label>
-          <button onClick={() => handleMealAmountButtonClick(-1)}>
-            <BsFileArrowDownFill />
-          </button>
-          <input
-            type="number"
-            id="number"
-            value={mealAmount}
-            onChange={handleMealAmountChange}
+    <animated.div style={props}>
+      <Card className={styles.meal}>
+        <img src={picture} alt={name} />
+        <h3 className={styles.mealName}>{name}</h3>
+        <p className={styles.mealDescription}>{truncateString(description)}</p>
+        <p className={styles.mealPrice}>${price.toFixed(2)}</p>
+        <div className={styles.mealAddSection}>
+          <div className={styles.mealAmount}>
+            <label htmlFor="number" defaultValue="1">
+              Amount
+            </label>
+            <button onClick={() => handleMealAmountButtonClick(-1)}>
+              <BsFileArrowDownFill />
+            </button>
+            <input
+              type="number"
+              id="number"
+              value={mealAmount}
+              onChange={handleMealAmountChange}
+            />
+            <button onClick={() => handleMealAmountButtonClick(1)}>
+              <BsFileArrowUpFill />
+            </button>
+          </div>
+          <IconButton
+            Icon={AiOutlinePlus}
+            label="Add"
+            onClick={handleAddMealToCart}
           />
-          <button onClick={() => handleMealAmountButtonClick(1)}>
-            <BsFileArrowUpFill />
-          </button>
         </div>
-        <IconButton
-          Icon={AiOutlinePlus}
-          label="Add"
-          onClick={handleAddMealToCart}
-        />
-      </div>
-    </Card>
+      </Card>
+    </animated.div>
   );
 }
 
