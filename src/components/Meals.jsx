7ce8a6c7b899dbbc9a5e6c4403ae8f94c3d2625e
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-04 18:30:40 +0200

    use rtk query to fetch data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,51 +1,24 @@
 import styles from "../styles/components/Meals.module.css";
 import globalStyles from "../styles/global.module.css";
 import Meal from "./Meal";
-import { useEffect, useState } from "react";
-import axiosInstance from "../api/axios";
+import { useGetMealsQuery } from "../slices/apiSlice";
 
 function Meals() {
-  const [meals, setMeals] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { data: meals, isLoading, error } = useGetMealsQuery();
 
-  useEffect(() => {
-    setLoading(true);
-    setError(null);
-    const controller = new AbortController();
-    let subscribed = true;
-    const getMeals = async () => {
-      try {
-        const response = await axiosInstance.get("/meals", {
-          signal: controller.signal,
-        });
-        if (subscribed) {
-          setMeals(response.data);
-          setError(null);
-        }
-      } catch (error) {
-        if (error.message === "canceled") return;
-        console.error(error);
-        setError(error);
-      }
-      setLoading(false);
-    };
+  let content;
 
-    getMeals();
-
-    return () => {
-      controller.abort();
-      subscribed = false;
-    };
-  }, []);
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (error) {
+    content = <p>Error: {error.data.message}</p>;
+  } else {
+    content = meals.map((meal) => <Meal key={meal._id} {...meal} />);
+  }
 
   return (
     <section className={`${styles.meals} ${globalStyles.container}`}>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {!loading &&
-        !error &&
-        meals.map((meal) => <Meal key={meal._id} {...meal} />)}
+      {content}
     </section>
   );
 }
