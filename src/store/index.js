5c0eadbf9df05abd506a32048a96d72f46ad8c49
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-04 18:31:35 +0200

    add mealApi reducer to the redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../slices/cartSlice";
 import modalReducer from "../slices/modalSlice";
+import { mealApi } from "../slices/apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     modal: modalReducer,
+    [mealApi.reducerPath]: mealApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(mealApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export default store;
