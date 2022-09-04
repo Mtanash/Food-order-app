071ac8c48cb4e554364183c1fe0e9e2492f06a6b
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-04 18:31:09 +0200

    refactor amountInput to a custom hook

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -2,16 +2,18 @@ import styles from "../styles/components/Meal.module.css";
 import Card from "../ui/Card";
 import IconButton from "../ui/IconButton";
 import { AiOutlinePlus } from "react-icons/ai";
-import { BsFileArrowUpFill, BsFileArrowDownFill } from "react-icons/bs";
 import truncateString from "../helpers/truncateString";
-import { useState } from "react";
 import toastNotify from "../helpers/toast";
 import { useSpring, animated } from "react-spring";
 import { useDispatch } from "react-redux";
 import { addItemToCart } from "../slices/cartSlice";
+import { Link } from "react-router-dom";
+import useAmount from "../hooks/useAmount";
+import MealAmountInput from "../ui/MealAmountInput";
 
-function Meal({ _id, name, description, picture, price }) {
-  const [mealAmount, setMealAmount] = useState(1);
+function Meal({ _id: id, name, description, picture, price }) {
+  const { mealAmount, handleMealAmountChange, handleMealAmountButtonClick } =
+    useAmount();
 
   const dispatch = useDispatch();
 
@@ -21,33 +23,10 @@ function Meal({ _id, name, description, picture, price }) {
     delay: 200,
   });
 
-  const handleMealAmountChange = (e) => {
-    if (+e.target.value < 1) {
-      setMealAmount(1);
-    } else {
-      setMealAmount(+e.target.value);
-    }
-  };
-
-  const handleMealAmountButtonClick = (amount) => {
-    if (amount === -1) {
-      if (mealAmount < 2) {
-        setMealAmount(1);
-      } else {
-        setMealAmount((prev) => prev - 1);
-      }
-    } else if (amount === 1) {
-      setMealAmount((prev) => prev + 1);
-    } else {
-      console.log("Please provide amount = -1 or 1");
-      return;
-    }
-  };
-
   const handleAddMealToCart = () => {
     const item = {
       data: {
-        id: _id,
+        id,
         name,
         description,
         price,
@@ -63,28 +42,18 @@ function Meal({ _id, name, description, picture, price }) {
   return (
     <animated.div style={props}>
       <Card className={styles.meal}>
-        <img src={picture} alt={name} />
-        <h3 className={styles.mealName}>{name}</h3>
+        <Link to={`/meals/${id}`}>
+          <img src={picture} alt={name} />
+          <h3 className={styles.mealName}>{name}</h3>
+        </Link>
         <p className={styles.mealDescription}>{truncateString(description)}</p>
         <p className={styles.mealPrice}>${price.toFixed(2)}</p>
         <div className={styles.mealAddSection}>
-          <div className={styles.mealAmount}>
-            <label htmlFor="number" defaultValue="1">
-              Amount
-            </label>
-            <button onClick={() => handleMealAmountButtonClick(-1)}>
-              <BsFileArrowDownFill />
-            </button>
-            <input
-              type="number"
-              id="number"
-              value={mealAmount}
-              onChange={handleMealAmountChange}
-            />
-            <button onClick={() => handleMealAmountButtonClick(1)}>
-              <BsFileArrowUpFill />
-            </button>
-          </div>
+          <MealAmountInput
+            mealAmount={mealAmount}
+            handleMealAmountChange={handleMealAmountChange}
+            handleMealAmountButtonClick={handleMealAmountButtonClick}
+          />
           <IconButton
             Icon={AiOutlinePlus}
             label="Add"
