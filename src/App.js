1c392a39620993f1a0654ccb73462e8eb5ee101c
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-04 18:24:30 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import CartModal from "./components/CartModal";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Meals from "./components/Meals";
 import { ToastContainer } from "react-toastify";
 import { selectCartItems } from "./slices/cartSlice";
 import { useSelector } from "react-redux";
 import { selectModalIsOpen } from "./slices/modalSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MealPage from "./pages/MealPage";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
@@ -19,23 +20,27 @@ function App() {
   }, [cartItems]);
 
   return (
-    <main>
-      <ToastContainer
-        position="top-center"
-        autoClose={2500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover={false}
-      />
-      <Header />
-      <Hero />
-      <Meals />
-      {modalIsOpen && createPortal(<CartModal />, document.body)}
-    </main>
+    <BrowserRouter>
+      <main>
+        <ToastContainer
+          position="top-center"
+          autoClose={2500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover={false}
+        />
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/meals/:mealId" element={<MealPage />} />
+        </Routes>
+        {modalIsOpen && createPortal(<CartModal />, document.body)}
+      </main>
+    </BrowserRouter>
   );
 }
 
